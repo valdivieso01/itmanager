7b78589fcb672df0136474dea5c5da442b18ad39
Javier <valdivieso01@gmail.com>
2019-03-18 23:56:40 -0300

    Se agrega messages a admin

--- messenger/admin.py
+++ messenger/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Message, ThreadManager, Thread
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    exclude = ('')
+
+class ThreadAdmin(admin.ModelAdmin):
+    exclude = ('')
+
+admin.site.register(Message, MessageAdmin)
+
+admin.site.register(Thread, ThreadAdmin)
+
+
